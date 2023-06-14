846a8c30d1e980870a21eeda269ed7a2958d53c4
Drissa <dcompte223@gmail.com>
2023-06-14 09:39:05 +0100

    Update schema_based_on_diagram.sql

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -18,3 +18,24 @@ CREATE TABLE medical_histories (
   patient_id INT REFERENCES patients(id),
   status VARCHAR(40)
 );
+
+CREATE TABLE invoice_items (
+  id INT PRIMARY KEY,
+  unit_price DECIMAL,
+  quantity INT,
+  total_price DECIMAL,
+  invoice_id INT REFERENCES invoices(id),
+  treatment_id INT REFERENCES treatments(id)
+);
+
+CREATE TABLE treatments (
+  id INT PRIMARY KEY,
+  name VARCHAR(30),
+  type VARCHAR(30)
+);
+
+CREATE TABLE treatment_histories (
+  id INT PRIMARY KEY,
+  treatment_id INT REFERENCES treatments(id),
+  medical_history_id INT REFERENCES medical_histories(id)
+);
